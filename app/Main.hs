0fa3f24e1d7b3482ee15a64baf14de1011720ce1
luyaomacs <luyaomacs@gmail.com>
2023-12-14 00:02:44 +0000

    update Main.hs, add startMenu

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,11 @@
 module Main (main) where
 
+import StartUI (startMenu)
 import GameUI (startGame)
 
 main :: IO ()
-main = startGame
+main = do
+    mode <- startMenu
+    case mode of
+        1 -> startGame
+        _ -> return ()
