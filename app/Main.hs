967170c66619332e8679528b136612e8a0dcdd36
Yimeng Wang <ymwang000@gmail.com>
2023-12-13 21:17:28 -0800

    modify quit logic

--- app/Main.hs
+++ app/Main.hs
@@ -3,13 +3,14 @@ module Main (main) where
 import StartUI (startMenu)
 import GameUI (startGame)
 import MusicUI (chooseMusic)
+import Control.Monad (replicateM_)
 
 main :: IO ()
-main = do
+main = replicateM_ 10 (do
   mode <- startMenu
   case mode of
     1 -> startGame
     2 -> do
       _ <- chooseMusic
       main
-    _ -> return ()
+    _ -> return ())
